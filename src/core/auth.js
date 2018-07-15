9a0b2374aa5e2f0a43f76fa5f7e2c8e51212315c
Wade121 <Wadesmith201@hotmail.com>
2018-07-15 12:16:44 +1000

    Create auth.js

---
+++ src/core/auth.js
@@ -0,0 +1,99 @@
+/*eslint-disable*/
+
+//
+
+// Add Your Discord Bot Token here
+
+// Discord Dev > My Apps > Bot > App Bot User > Token > Reveal
+
+// https://discordapp.com/developers/applications/me
+
+//
+
+
+
+exports.token = "";
+
+
+
+//
+
+// Add your Discord Main User ID here
+
+// In Discord, Go to Settings > Appearance > Enable Developer Mode
+
+// Right click your user in channel/message and pick "Copy ID" to obtain
+
+//
+
+
+
+exports.botOwner = "";
+
+
+
+//
+
+// Invite URL (OAuth2)
+
+//
+
+
+
+exports.invite = "";
+
+
+
+//
+
+// Add Webhook URL for logging (optional)
+
+//
+
+
+
+exports.loggerWebhook = null;
+
+
+
+//
+
+// Add donation URL (optional)
+
+//
+
+
+
+exports.donation = null;
+
+
+
+//
+
+// Changelog URL (optional)
+
+//
+
+
+
+exports.changelog = null;
+
+
+
+//
+
+// Developer Mode
+
+//
+
+
+
+exports.dev = false;
+
+
+
+//
+
+// Remember to rename this file to "auth.js" after you're done
+
+//
